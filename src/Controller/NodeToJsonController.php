229c22d4efda850076d368195bd7a526693d192b
jasson <jeirod@eltiempo.com>
2019-05-28 15:43:25 -0500

    [limpieza] se limpia el modulo

--- src/Controller/NodeToJsonController.php
+++ src/Controller/NodeToJsonController.php
@@ -9,17 +9,4 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class NodeToJsonController extends ControllerBase {
 
-	/**
-	 * Display the markup.
-	 *
-	 * @return array
-	 *   Return markup array.
-	 */
-	public function content() {
-		return [
-			'#type' => 'markup',
-			'#markup' => $this->t('Hello, etce World!'),
-		];
-	}
-
 }
